16f10d46f964956815d1da9f96e8b14a8520472e
nc-minh <nguyencongminh.official@gmail.com>
2022-12-13 14:34:58 +0700

    :green_apple: first test

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	arg := CreateAccountParams{
+		Owner:    "test owner",
+		Balance:  10,
+		Currency: "USD",
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.CreatedAt)
+	require.NotZero(t, account.ID)
+}
